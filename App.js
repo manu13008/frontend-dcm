615ee1ba5603ad1bf0acdb31d7794eb9e353330b
Manu <emmanuel.ayad@gmail.com>
2024-05-20 09:59:19 +0200

    Initial depot

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+
+
+// Persistance du store
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers} from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const reducers = combineReducers({ user });
+const persistConfig = { key: 'dcm-app',   storage: AsyncStorage, };
+
+
+// Persistance du store
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+ });
+ const persistor = persistStore(store);
+
 
 export default function App() {
+
+  const store = configureStore({
+    reducer: {},
+    });
+    
+    const persistConfig = {
+      key: 'root',
+      storage: AsyncStorage,
+    }
+
   return (
+    <Provider store={store}>
+ <PersistGate persistor={persistor}>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </PersistGate>
+    </Provider>
   );
 }
 
