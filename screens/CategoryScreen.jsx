043bd1bd6f6dd3e80e9469f664eb169fcb8fd8bf
rinocerosa <anaischaouati@gmail.com>
2024-05-21 17:36:38 +0200

    kk

--- screens/CategoryScreen.jsx
+++ screens/CategoryScreen.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-
+import Header from "../components/Header";
 const CategoryScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>CategorieScreen</Text>
-    </View>
-  );
-};
+
+    return (
+      <View style={styles.container}>
+        <Header showButton={false} />
+        <Text>CategorieScreen</Text>
+      </View>
+    );
+  };
 
 const styles = StyleSheet.create({
   container: {
